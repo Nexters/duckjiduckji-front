36cd076b064d40bd78b15532f8f479641a32f045
changicho <changicho29@gmail.com>
2022-01-29 15:50:07 +0900

    feat: Add recoil

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -2,6 +2,14 @@ import type { AppProps } from "next/app";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from "recoil";
+
 const GlobalStyle = createGlobalStyle`
   ${reset}
   body {
@@ -11,10 +19,10 @@ const GlobalStyle = createGlobalStyle`
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <RecoilRoot>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </RecoilRoot>
   );
 }
 
