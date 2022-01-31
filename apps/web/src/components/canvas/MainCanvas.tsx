1c52dadad811f214a374463505ba3211cd299f02
kim-taewoo <acoustic0419@gmail.com>
2022-02-01 02:17:17 +0900

    added types for handlers in MainCanavs

--- apps/web/src/components/canvas/MainCanvas.tsx
+++ apps/web/src/components/canvas/MainCanvas.tsx
@@ -1,19 +1,25 @@
-import { useRef, useState } from 'react';
-import { useKeyPressEvent, useWindowSize } from 'react-use';
+import { useRef } from 'react';
+import { useWindowSize } from 'react-use';
 import { Stage, Layer } from 'react-konva';
 import Konva from 'konva';
 import { Polaroids, PostIts } from 'web/src/components/canvas/shapes';
 import { useRecoilState } from 'recoil';
 import { shapesState, userActionState } from 'web/src/atoms';
+import { KonvaEventObject } from 'konva/lib/Node';
 
 const SCALE_BY = 1.01;
 Konva.hitOnDragEnabled = true;
 
-function getDistance(p1, p2) {
+type Coordinates = {
+  x: number;
+  y: number;
+};
+
+function getDistance(p1: Coordinates, p2: Coordinates) {
   return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
 }
 
-function getCenter(p1, p2) {
+function getCenter(p1: Coordinates, p2: Coordinates) {
   return {
     x: (p1.x + p2.x) / 2,
     y: (p1.y + p2.y) / 2,
@@ -29,28 +35,11 @@ function MainCanvas({}: Props) {
   const [shapes, setShapes] = useRecoilState(shapesState);
   const [userAction, setUserAction] = useRecoilState(userActionState);
   const { width, height } = useWindowSize();
-  const [isStageDraggable, setIsStageDraggable] = useState(false);
-  const [cursor, setCursor] = useState<string>('auto');
   const stageRef = useRef(null);
-  useKeyPressEvent(
-    ' ',
-    () => {
-      setIsStageDraggable(true);
-      setCursor('grab');
-    },
-    () => {
-      setIsStageDraggable(false);
-      setCursor('auto');
-    },
-  );
-  const inputRef = useRef(null);
-  const [inputStyle, setInputStyle] = useState<any>({
-    background: 'red',
-  });
 
   const isShapesDraggable = userAction === 'pinch' ? false : true;
 
-  function handleStageWheel(e) {
+  function handleStageWheel(e: KonvaEventObject<WheelEvent>) {
     e.evt.preventDefault();
     const stage = stageRef.current;
     if (!stage) return;
@@ -61,8 +50,11 @@ function MainCanvas({}: Props) {
       y: (pointerY - stageRef.current.y()) / oldScale,
     };
 
-    // TODO: SCALE_BY 라는 상수대신 휠의 속도를 고려한 deltaY 를 활용해야 할 듯
-    const newScale = e.evt.deltaY < 0 ? oldScale * SCALE_BY : oldScale / SCALE_BY;
+    // deltaY 크기로 스크롤 줌인/아웃 속도 조절
+    const newScale =
+      e.evt.deltaY < 0
+        ? oldScale * (SCALE_BY + Math.abs(e.evt.deltaY) / 5000)
+        : oldScale / (SCALE_BY + Math.abs(e.evt.deltaY) / 5000);
     stageRef.current.scale({ x: newScale, y: newScale });
     const newPos = {
       x: pointerX - mousePointTo.x * newScale,
@@ -72,109 +64,84 @@ function MainCanvas({}: Props) {
     stageRef.current.batchDraw();
   }
 
-  function handleDblTap(e) {
+  function handleDblTap(e: KonvaEventObject<Event>) {
     alert(JSON.stringify(stageRef.current.getPointerPosition()));
   }
 
-  const spanRef = useRef(null);
-  const [inputValue, setInputValue] = useState('');
-  console.log(stageRef.current?.scaleX());
+  function handleStageTouchMove(e: KonvaEventObject<TouchEvent>) {
+    e.evt.preventDefault();
+    const touch1 = e.evt.touches[0];
+    const touch2 = e.evt.touches[1];
+    if (!touch1 || !touch2) return;
+    // if the stage was under Konva's drag&drop
+    // we need to stop it, and implement our own pan logic with two pointers
+    if (stageRef.current.isDragging()) {
+      stageRef.current.stopDrag();
+    }
+
+    // prevent shapes from dragging while pinch zoom
+    setUserAction('pinch');
+
+    const p1 = {
+      x: touch1.clientX,
+      y: touch1.clientY,
+    };
+    const p2 = {
+      x: touch2.clientX,
+      y: touch2.clientY,
+    };
+
+    if (!lastCenter) {
+      lastCenter = getCenter(p1, p2);
+      return;
+    }
+    const newCenter = getCenter(p1, p2);
+    const dist = getDistance(p1, p2);
+    if (!lastDist) {
+      lastDist = dist;
+    }
+    // local coordinates of center point
+    const pointTo = {
+      x: (newCenter.x - stageRef.current.x()) / stageRef.current.scaleX(),
+      y: (newCenter.y - stageRef.current.y()) / stageRef.current.scaleX(),
+    };
+
+    const scale = stageRef.current.scaleX() * (dist / lastDist);
+    stageRef.current.scaleX(scale);
+    stageRef.current.scaleY(scale);
+
+    // calculate new position of the stage
+    const dx = newCenter.x - lastCenter.x;
+    const dy = newCenter.y - lastCenter.y;
+
+    const newPos = {
+      x: newCenter.x - pointTo.x * scale + dx,
+      y: newCenter.y - pointTo.y * scale + dy,
+    };
+
+    stageRef.current.position(newPos);
+    lastDist = dist;
+    lastCenter = newCenter;
+  }
+
+  function handleStageTouchEnd() {
+    lastDist = 0;
+    lastCenter = null;
+    setUserAction('browse');
+  }
+
   return (
-    <div style={{ cursor }}>
-      {/* <input
-        type="text"
-        onChange={e => setInputValue(e.currentTarget.value)}
-        style={{
-          fontSize: 22,
-          position: 'fixed',
-          top: 200,
-          left: 100,
-          zIndex: 1,
-          opacity: 1,
-          margin: 0,
-          padding: 0,
-          border: 0,
-          outline: 'none',
-          textDecoration: 'none',
-          background: 'rgba(0, 0, 0, 0)',
-        }}
-      />
-      <span ref={spanRef} style={{ display: 'none', position: 'fixed' }}>
-        {inputValue}
-      </span> */}
+    <div>
       <Stage
         ref={stageRef}
         draggable={true}
-        width={width ?? 300}
-        height={height ?? 300}
+        width={width}
+        height={height}
         onWheel={handleStageWheel}
         onDblTap={handleDblTap}
-        onTouchEnd={e => {
-          lastDist = 0;
-          lastCenter = null;
-          setUserAction('browse');
-        }}
-        onTouchMove={e => {
-          e.evt.preventDefault();
-          const touch1 = e.evt.touches[0];
-          const touch2 = e.evt.touches[1];
-
-          if (touch1 && touch2) {
-            // if the stage was under Konva's drag&drop
-            // we need to stop it, and implement our own pan logic with two pointers
-            if (stageRef.current.isDragging()) {
-              stageRef.current.stopDrag();
-            }
-
-            setUserAction('pinch');
-
-            const p1 = {
-              x: touch1.clientX,
-              y: touch1.clientY,
-            };
-            const p2 = {
-              x: touch2.clientX,
-              y: touch2.clientY,
-            };
-
-            if (!lastCenter) {
-              lastCenter = getCenter(p1, p2);
-              return;
-            }
-            const newCenter = getCenter(p1, p2);
-
-            const dist = getDistance(p1, p2);
-
-            if (!lastDist) {
-              lastDist = dist;
-            }
-
-            // local coordinates of center point
-            const pointTo = {
-              x: (newCenter.x - stageRef.current.x()) / stageRef.current.scaleX(),
-              y: (newCenter.y - stageRef.current.y()) / stageRef.current.scaleX(),
-            };
-
-            const scale = stageRef.current.scaleX() * (dist / lastDist);
-
-            stageRef.current.scaleX(scale);
-            stageRef.current.scaleY(scale);
-
-            // calculate new position of the stage
-            const dx = newCenter.x - lastCenter.x;
-            const dy = newCenter.y - lastCenter.y;
-
-            const newPos = {
-              x: newCenter.x - pointTo.x * scale + dx,
-              y: newCenter.y - pointTo.y * scale + dy,
-            };
-
-            stageRef.current.position(newPos);
-
-            lastDist = dist;
-            lastCenter = newCenter;
-          }
-        }}>
+        onTouchEnd={handleStageTouchEnd}
+        onTouchMove={handleStageTouchMove}
+      >
         <Layer>
           <PostIts postIts={shapes.postIts} isDraggable={isShapesDraggable} />
           <Polaroids polaroids={shapes.polaroids} isDraggable={isShapesDraggable} />
