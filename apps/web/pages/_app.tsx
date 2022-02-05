f4a6608d19aeb14c2ecd8706996399a190d811c1
changicho <changicho29@gmail.com>
2022-02-05 14:24:38 +0900

    feat: Add font-face in global style

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -15,6 +15,13 @@ const GlobalStyle = createGlobalStyle`
   body {
     overflow: hidden;
   }
+
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
 `;
 
 function App({ Component, pageProps }: AppProps) {
