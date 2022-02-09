5e164dcc794aabea8313ee64ec53a4417a716840
changicho <changicho29@gmail.com>
2022-02-09 22:41:18 +0900

    refactor: Remove useless import

--- apps/web/pages/login.tsx
+++ apps/web/pages/login.tsx
@@ -1,5 +1,4 @@
 import { LoginPage as InnerComponent } from 'web/components/LoginPage';
-import { default as HomeComponent } from 'web/components/Home';
 
 function LoginPage() {
   return <InnerComponent />;
