8c9430f4428afdb7433209b7cd5aabf0bb901088
changicho <changicho29@gmail.com>
2022-02-16 22:45:47 +0900

    feat: Add valid check method & warning text

--- apps/web/components/board/CreateBoardModal.tsx
+++ apps/web/components/board/CreateBoardModal.tsx
@@ -1,3 +1,4 @@
+import { ChangeEventHandler, useEffect } from 'react';
 import { useState, useRef, MouseEventHandler, type FunctionComponent } from 'react';
 import styled from 'styled-components';
 
@@ -5,17 +6,37 @@ interface Props {
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+const checkValid = (title: string) => {
+  return title.length <= 20;
+};
+
 export const CreateBoardModal: FunctionComponent<Props> = ({ setOpen }) => {
   const inputRef = useRef<HTMLInputElement>();
 
   const [isActive, setActive] = useState<boolean>(false);
+  const [isFail, setFail] = useState<boolean>(false);
 
   const closeModal: MouseEventHandler<HTMLButtonElement> = () => {
     setOpen(false);
   };
 
-  const createBoard = () => {
-    console.log(inputRef.current.value);
+  const onType: ChangeEventHandler<HTMLInputElement> = event => {
+    const { value } = event.target;
+
+    if (!checkValid(value)) {
+      setFail(true);
+      setActive(false);
+      return;
+    }
+
+    if (value.length > 0) setActive(true);
+    setFail(false);
+  };
+
+  const createBoard: MouseEventHandler<HTMLButtonElement> = () => {
+    const { value } = inputRef.current;
+
+    if (!checkValid(value)) return;
   };
 
   return (
@@ -23,8 +44,16 @@ export const CreateBoardModal: FunctionComponent<Props> = ({ setOpen }) => {
       <EditContainer>
         <CloseButton onClick={closeModal}>✕</CloseButton>
         <Information>보드 이름을 설정해 주세요</Information>
-        <Input ref={inputRef} type="text"></Input>
-        <ConfirmButton className={true && 'active'}>완료</ConfirmButton>
+        <Input
+          ref={inputRef}
+          onChange={onType}
+          className={(isActive && 'active') || (isFail && 'fail')}
+          type="text"
+        ></Input>
+        {isFail && <Warn>20자 이하로 입력해주세요</Warn>}
+        <ConfirmButton onClick={createBoard} className={isActive && 'active'}>
+          완료
+        </ConfirmButton>
       </EditContainer>
     </Wrapper>
   );
@@ -52,7 +81,7 @@ const EditContainer = styled.div`
 
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  /* justify-content: center; */
   align-items: center;
 
   background: #ffffff;
@@ -62,8 +91,8 @@ const EditContainer = styled.div`
 const Information = styled.span`
   width: 360px;
   height: 36px;
-
-  margin-top: 80px;
+  position: absolute;
+  top: 80px;
 
   font-family: Pretendard;
   font-style: normal;
@@ -88,7 +117,7 @@ const Input = styled.input`
   width: 380px;
   height: 52px;
 
-  margin-top: 64px;
+  margin-top: 180px;
 
   font-family: Pretendard;
   font-style: normal;
@@ -100,16 +129,25 @@ const Input = styled.input`
 
   color: #a09fa6;
 
-  border-bottom: 2px solid #a09fa6; ;
+  border-bottom: 2px solid #a09fa6;
+
+  &.active {
+    border-bottom: 2px solid #6038ff;
+  }
+
+  &.fail {
+    border-bottom: 2px solid #fe2626;
+  }
 `;
 
 const ConfirmButton = styled.button`
   width: 380px;
   height: 64px;
 
-  border: none;
+  position: absolute;
+  bottom: 40px;
 
-  margin-top: 86px;
+  border: none;
 
   font-family: Pretendard;
   font-style: normal;
@@ -128,6 +166,10 @@ const ConfirmButton = styled.button`
   &.active {
     background: #6038ff;
   }
+
+  &.fail {
+    background: #fe2626;
+  }
 `;
 
 const CloseButton = styled.button`
@@ -145,4 +187,21 @@ const CloseButton = styled.button`
 
   font-size: 20px;
   line-height: 20px;
+`;
+
+const Warn = styled.p`
+  width: 380px;
+  height: 24px;
+
+  margin-top: 12px;
+
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 24px;
+
+  letter-spacing: 0.015em;
+
+  color: #fe2626;
 `;
