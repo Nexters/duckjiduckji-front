e572beeeb36416be044983cb0ec76d1b6d521c48
1ilsang <1ilsang@naver.com>
2022-02-17 11:24:23 +0900

    chore: Apply eslint rule

--- apps/web/components/editor/PolaroidHTML.tsx
+++ apps/web/components/editor/PolaroidHTML.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Wrapper = styled.div`
   position: absolute;
@@ -14,8 +14,7 @@ const Wrapper = styled.div`
   height: 528px;
 
   background-color: #c9c9c9;
-  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
-    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
     rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
 `;
 
