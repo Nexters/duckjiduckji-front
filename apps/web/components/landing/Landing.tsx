3e7be4fc67ae549ea1207143db8ffff801ebc669
changicho <changicho29@gmail.com>
2022-02-19 22:58:28 +0900

    feat: Add button arrow and fix layout
    버튼이 눌리지 않는 문제가 있어 레이아웃 수정함

--- apps/web/components/landing/Landing.tsx
+++ apps/web/components/landing/Landing.tsx
@@ -2,20 +2,51 @@ import { useRouter } from 'next/router';
 import { MouseEventHandler } from 'react';
 import styled from 'styled-components';
 
+export function Landing() {
+  const router = useRouter();
+
+  const moveRoomsPage: MouseEventHandler<HTMLButtonElement> = () => {
+    router.push('/board');
+  };
+
+  return (
+    <Wrapper>
+      <BackgroundUpper />
+      <BackgroundDown />
+      <Inner>
+        <MainText>
+          <ImpactText backgroundImage="assets/image/green_panel.png">덕지덕지</ImpactText> 붙여봐요!
+          <br />
+          우리만의 추억을 담는 <ImpactText backgroundImage="assets/image/yellow_panel.png">콜렉트북</ImpactText>
+          <Arrow />
+        </MainText>
+        <SubText>
+          스쳐 지나가는 우리의 일상들, 친구들과 함께 추억하고 싶지 않으셨나요?
+          <br />
+          타임캡슐 공간을 만들어 사진과 글을 실시간으로 꾸미고 공유해보세요!
+        </SubText>
+        <LinkButton onClick={moveRoomsPage}>
+          덕지덕지 시작하기 <ButtonArrow />
+        </LinkButton>
+      </Inner>
+    </Wrapper>
+  );
+}
+
 const Wrapper = styled.div`
+  z-index: -1;
   width: 100vw;
   height: 100vh;
+`;
+
+const Inner = styled.div`
+  width: 100%;
+  height: 100%;
 
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
-  background: #0f0b1b;
-  background-repeat: no-repeat;
-  background-size: contain;
-  background-attachment: fixed;
-  background-position: center bottom;
 `;
 
 const MainText = styled.span`
@@ -59,8 +90,13 @@ const LinkButton = styled.button`
   align-items: center;
   padding: 16px 36px;
 
-  width: 202px;
+  width: 252px;
   height: 64px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 
   background: #6038ff;
   border-radius: 32px;
@@ -68,7 +104,8 @@ const LinkButton = styled.button`
   font-family: Pretendard;
   font-style: normal;
   font-weight: bold;
-  font-size: 16px;
+  font-size: 18px;
+  line-height: 24px;
 
   text-align: center;
   letter-spacing: 0.015em;
@@ -83,36 +120,11 @@ const ImpactText = styled.span<{ backgroundImage: string }>`
   padding: 15px;
 `;
 
-export function Landing() {
-  const router = useRouter();
-
-  const moveRoomsPage: MouseEventHandler<HTMLButtonElement> = () => {
-    router.push('/board');
-  };
-
-  return (
-    <Wrapper>
-      <MainText>
-        <ImpactText backgroundImage="assets/image/green_panel.png">덕지덕지</ImpactText> 붙여봐요!
-        <br />
-        우리만의 추억을 담는 <ImpactText backgroundImage="assets/image/yellow_panel.png">콜렉트북</ImpactText>
-        <Arrow />
-      </MainText>
-      <SubText>
-        스쳐 지나가는 우리의 일상들, 친구들과 함께 추억하고 싶지 않으셨나요?
-        <br />
-        타임캡슐 공간을 만들어 사진과 글을 실시간으로 꾸미고 공유해보세요!
-      </SubText>
-      <LinkButton onClick={moveRoomsPage}>덕지덕지 시작하기</LinkButton>
-      <BackgroundUpper />
-      <BackgroundDown />
-    </Wrapper>
-  );
-}
-
 const BackgroundUpper = styled.div`
-  width: 100vw;
-  height: 100vh;
+  background: #0f0b1b;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
   position: absolute;
   top: 0;
 
@@ -124,8 +136,9 @@ const BackgroundUpper = styled.div`
 `;
 
 const BackgroundDown = styled.div`
-  width: 100vw;
-  height: 100vh;
+  z-index: -1;
+  width: 100%;
+  height: 100%;
   position: absolute;
   bottom: 0;
 
@@ -140,11 +153,19 @@ const Arrow = styled.div`
   position: absolute;
   width: 30px;
   height: 30px;
-
   right: 40px;
   bottom: -10px;
 
   background-image: url('assets/image/background_arrow.png');
+  background-repeat: no-repeat;
+  background-size: contain;
+`;
+
+const ButtonArrow = styled.p`
+  width: 24px;
+  height: 24px;
+  margin-left: 10px;
+  background-image: url('assets/image/button_arrow.png');
   background-repeat: no-repeat;
   background-size: contain;
 `;
