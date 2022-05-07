f014c03f0f05a2721adb6062f12f162117fa19d9
kim-taewoo <acoustic0419@gmail.com>
2022-05-07 19:28:22 +0900

    hoist only next js

--- apps/mobile/pages/index.tsx
+++ apps/mobile/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-gray-500">
       <div className="text-blue-500">hi</div>
     </div>
   );
